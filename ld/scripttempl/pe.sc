davidwed/cegcc

* ld/emultempl/pe.em: Change default to pseudo-relocation v2. * ld/scripttempl/pe.sc: Move edata and idata section contents into the   rdata section, and remove the edata and idata sections. * ld/pe-dll.c (ERVA): Change macro definition to take edata section   offset into account. * bfd/cofflink.c: Remove commented out old command. * bfd/peXXigen.c (_bfd_XXi_swap_aouthdr_out): Save and restore the   PE_EXPORT_TABLE field of DataDirectory too. * ld/scripttempl/pe.sc: Add symbols __idata_start__, __idata_end__,   __edata_start__, __edata_end__ that _bfd_XXi_swap_aouthdr_out will   look for. * bfd/peXXigen.c (_bfd_XXi_swap_aouthdr_out): Get the newly defined   symbols like __edata_start__ and use them to initialize the   DataDirectory entries for PE_EXPORT_TABLE and PE_IMPORT_TABLE.

trunk/cegcc/src/binutils/ld/scripttempl/pe.sc
@@ -113,18 +113,36 @@ SECTIONS
 
   .rdata ${RELOCATING+BLOCK(__section_alignment__)} :
   {
+  /* Start original .rdata */
     ${R_RDATA}
     ${RELOCATING+___RUNTIME_PSEUDO_RELOC_LIST__ = .;}
     ${RELOCATING+__RUNTIME_PSEUDO_RELOC_LIST__ = .;}
     *(.rdata_runtime_pseudo_reloc)
     ${RELOCATING+___RUNTIME_PSEUDO_RELOC_LIST_END__ = .;}
     ${RELOCATING+__RUNTIME_PSEUDO_RELOC_LIST_END__ = .;}
+  /* End original .rdata */
+
+  ${RELOCATING+. = ALIGN(16);}
 
   /* Moved .edata */
+    ${RELOCATING+__edata_start__ = .;}
+    *(.edata)
+    ${RELOCATING+__edata_end__ = .;}
   /* End moved .edata */
 
+
+  ${RELOCATING+. = ALIGN(16);}
+
   /* Moved .idata */
-	/* Moving idata appears to cause a crash even in d2.exe */
+    /* This cannot currently be handled with grouped sections.
+	See pe.em:sort_sections.  */
+    ${RELOCATING+__idata_start__ = .;}
+    ${R_IDATA234}
+    ${RELOCATING+__idata5_start__ = .;}
+    ${R_IDATA5}
+    ${RELOCATING+__idata5_end__ = .;}
+    ${R_IDATA67}
+    ${RELOCATING+__idata_end__ = .;}
   /* End moved .idata */
   }
 
@@ -138,28 +156,12 @@ SECTIONS
     *(.pdata)
   }
 
-  .edata ${RELOCATING+BLOCK(__section_alignment__)} :
-  {
-    *(.edata)
-  }
-
   /DISCARD/ :
   {
     *(.debug\$S)
     *(.debug\$T)
     *(.debug\$F)
     *(.drectve)
-  }
-
-  .idata ${RELOCATING+BLOCK(__section_alignment__)} :
-  {
-    /* This cannot currently be handled with grouped sections.
-	See pe.em:sort_sections.  */
-    ${R_IDATA234}
-    ${RELOCATING+__idata5_start__ = .;}
-    ${R_IDATA5}
-    ${RELOCATING+__idata5_end__ = .;}
-    ${R_IDATA67}
   }
 
   .CRT ${RELOCATING+BLOCK(__section_alignment__)} :

MaxKellermann/binutils-gdb

git-svn-id: https://cegcc.svn.sourceforge.net/svnroot/cegcc/trunk/cegcc/src/binutils@1418 d7810a3d-100a-0410-8641-c3624a9c11f1

ld/scripttempl/pe.sc
@@ -123,16 +123,34 @@ SECTIONS
 
   .rdata ${RELOCATING+BLOCK(__section_alignment__)} :
   {
+  /* Start original .rdata */
     ${R_RDATA}
     ${RELOCATING+__rt_psrelocs_start = .;}
     KEEP(*(.rdata_runtime_pseudo_reloc))
     ${RELOCATING+__rt_psrelocs_end = .;}
+  /* End original .rdata */
+
+  ${RELOCATING+. = ALIGN(16);}
 
   /* Moved .edata */
+    ${RELOCATING+__edata_start__ = .;}
+    *(.edata)
+    ${RELOCATING+__edata_end__ = .;}
   /* End moved .edata */
 
+
+  ${RELOCATING+. = ALIGN(16);}
+
   /* Moved .idata */
-	/* Moving idata appears to cause a crash even in d2.exe */
+    /* This cannot currently be handled with grouped sections.
+	See pe.em:sort_sections.  */
+    ${RELOCATING+__idata_start__ = .;}
+    ${R_IDATA234}
+    ${RELOCATING+__IAT_start__ = .;}
+    ${R_IDATA5}
+    ${RELOCATING+__IAT_end__ = .;}
+    ${R_IDATA67}
+    ${RELOCATING+__idata_end__ = .;}
   /* End moved .idata */
   }
   ${RELOCATING+__rt_psrelocs_size = __rt_psrelocs_end - __rt_psrelocs_start;}
@@ -159,11 +177,6 @@ SECTIONS
     ${RELOCATING+__bss_end__ = . ;}
   }
 
-  .edata ${RELOCATING+BLOCK(__section_alignment__)} :
-  {
-    *(.edata)
-  }
-
   /DISCARD/ :
   {
     *(.debug\$S)
@@ -174,16 +187,6 @@ SECTIONS
     ${RELOCATING+ *(.gnu.lto_*)}
   }
 
-  .idata ${RELOCATING+BLOCK(__section_alignment__)} :
-  {
-    /* This cannot currently be handled with grouped sections.
-	See pe.em:sort_sections.  */
-    ${R_IDATA234}
-    ${RELOCATING+__IAT_start__ = .;}
-    ${R_IDATA5}
-    ${RELOCATING+__IAT_end__ = .;}
-    ${R_IDATA67}
-  }
   .CRT ${RELOCATING+BLOCK(__section_alignment__)} :
   { 					
     ${RELOCATING+___crt_xc_start__ = . ;}