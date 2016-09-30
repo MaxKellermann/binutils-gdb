31a0bef0c74be8cbd33a0efcfe94c65fbad6c4b4
Max Kellermann <max.kellermann@gmail.com>
2016-09-30 17:41:01 +0200

    gas/config/tc-arm: disable ".def" on COFF
    It conflicts with the COFF ".def" command.

--- gas/config/tc-arm.c
+++ gas/config/tc-arm.c
@@ -3156,6 +3156,8 @@ s_ccs_endasmfunc (int unused ATTRIBUTE_UNUSED)
     as_bad (_(".endasmfunc pseudo-op only available with -mccs flag."));
 }
 
+#ifndef OBJ_COFF
+
 static void
 s_ccs_def (int name)
 {
@@ -3164,6 +3166,8 @@ s_ccs_def (int name)
   else
     as_bad (_(".def pseudo-op only available with -mccs flag."));
 }
+
+#endif
 
 /* Directives: Literal pools.  */
 
@@ -4787,7 +4791,10 @@ const pseudo_typeS md_pseudo_table[] =
 
   /* These are for compatibility with CodeComposer Studio.  */
   {"ref",          s_ccs_ref,        0},
+#ifndef OBJ_COFF
+  /* this conflicts with the COFF ".def" command */
   {"def",          s_ccs_def,        0},
+#endif
   {"asmfunc",      s_ccs_asmfunc,    0},
   {"endasmfunc",   s_ccs_endasmfunc, 0},
 
